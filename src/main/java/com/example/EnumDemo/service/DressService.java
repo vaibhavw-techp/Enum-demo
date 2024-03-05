934f20e86a17b8a4d905c14705bd1416c514c762
vaibhav <vaibhav.waghmare@techprescient.com>
2024-03-05 15:39:29 +0530

    Added service class for DressController

---
+++ src/main/java/com/example/EnumDemo/service/DressService.java
@@ -0,0 +1,34 @@
+package com.example.EnumDemo.service;
+
+import com.example.EnumDemo.dto.DressDto;
+import com.example.EnumDemo.entity.DressEntity;
+import com.example.EnumDemo.mapper.DressMapper;
+import com.example.EnumDemo.repository.DressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class DressService {
+
+    @Autowired
+    private DressMapper dressMapper;
+
+    @Autowired
+    private DressRepository dressRepository;
+
+    public DressDto addDress(@RequestBody DressDto dressDto){
+        DressEntity dressEntity = new DressEntity();
+        dressEntity = dressMapper.dtoToEntity(dressDto);
+        dressRepository.save(dressEntity);
+        return dressDto;
+    }
+
+    public List<DressEntity> showDresses(){
+        return dressRepository.findAll()
+                .stream().collect(Collectors.toList());
+    }
+}
