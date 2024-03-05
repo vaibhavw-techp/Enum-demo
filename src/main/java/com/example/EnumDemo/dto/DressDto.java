8d82af4565711944f331b0aef518d5116b089ac8
vaibhav <vaibhav.waghmare@techprescient.com>
2024-03-05 15:39:46 +0530

    Added swagger UI and made minor changes

--- src/main/java/com/example/EnumDemo/dto/DressDto.java
+++ src/main/java/com/example/EnumDemo/dto/DressDto.java
@@ -2,7 +2,13 @@ package com.example.EnumDemo.dto;
 
 import com.example.EnumDemo.enumC.Color;
 import com.example.EnumDemo.enumC.Type;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DressDto {
 
     private String brand;
