e4703848db92dafe13894fc7bebcf68fa03a175b
vaibhav <vaibhav.waghmare@techprescient.com>
2024-03-07 14:40:43 +0530

    Removed constructor of enum to use enums in swagger

--- src/main/java/com/example/EnumDemo/enumC/Color.java
+++ src/main/java/com/example/EnumDemo/enumC/Color.java
@@ -1,24 +1,13 @@
 package com.example.EnumDemo.enumC;
 
 public enum Color {
-    WHITE("white"),
-    BLACK("Black"),
-    YELLOW("Yellow"),
-    PINK("Pink"),
-    GREEN("Green"),
-    GREY("Grey"),
-    RED("Red"),
-    BLUE("Blue");
-
-    private String value;
-
-    private Color(String value) {
-        this.value = value;
-    }
-
-    @Override
-    public String toString() {
-        return value;
-    }
+    WHITE,
+    BLACK,
+    YELLOW,
+    PINK,
+    GREEN,
+    GREY,
+    RED,
+    BLUE
 
 }
--- src/main/java/com/example/EnumDemo/enumC/Type.java
+++ src/main/java/com/example/EnumDemo/enumC/Type.java
@@ -1,22 +1,11 @@
 package com.example.EnumDemo.enumC;
 
 public enum Type {
-    TSHIRT("Tshirt"),
-    FORMAL("Formal"),
-    JACKET("Jacket"),
-    JERKIN("Jerkin"),
-    POLO("Polo"),
-    HOODY("Hoody"),
-    TURTLE_NECK("Turtleneck");
-
-    private String value;
-
-    private Type(String value) {
-        this.value = value;
-    }
-
-    @Override
-    public String toString() {
-        return value;
-    }
+    TSHIRT,
+    FORMAL,
+    JACKET,
+    JERKIN,
+    POLO,
+    HOODY,
+    TURTLE_NECK
 }
